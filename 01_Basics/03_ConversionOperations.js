4b9bea7f434e5dd2299f8d9bfadafbd6da383412
siddhika1436 <siddhika679@gmail.com>
2025-05-31 14:08:53 +0000

    ConversionOperations

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,29 @@
+let score = "35abc"
+let a = "32"
+let b = null
+let c = undefined
+
+console.log(typeof (score));
+console.log(typeof score);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c)
+
+
+let scoreNumber = Number(score)
+let aNum = Number(a)
+let bNum = Number(b)
+let cNum = Number(c)
+
+console.log(typeof scoreNumber)
+console.log(typeof aNum)
+console.log(typeof bNum);
+console.log(typeof cNum);
+
+
+
+console.log(scoreNumber)       //NaN => not a number
+console.log(aNum);
+console.log(bNum);
+console.log(cNum);
+
