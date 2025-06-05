54eecef1c5fe97376cf0103583d726a19a97746a
siddhika1436 <siddhika679@gmail.com>
2025-06-05 13:06:41 +0000

    Basics of strings and Arrays

---
+++ 01_Basics/04_Strings.js
@@ -0,0 +1,8 @@
+const str = "     siddhika        "
+console.log(str);
+console.log(str.trim());
+
+const url = "https://siddhika.com/siddhika%20rajput"
+
+
+console.log(url.replace('%20','-'));
---
+++ 02_Basics2/01_Arrays1.js
@@ -0,0 +1,9 @@
+const myArray = [0,1,2,3,4,5]
+
+//Array Methods
+myArray.push(6);
+console.log(myArray);
+
+console.log(Array.from("siddhika"));
+
+console.log(Array.from({name:"siddhika"}));
