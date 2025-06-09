182f5b4aa85efe61d6d69d75b506c9d6a896a9ea
siddhika1436 <siddhika679@gmail.com>
2025-06-09 12:09:18 +0000

    objects

--- 02_Basics2/02_Objects.js
+++ 02_Basics2/02_Objects.js
@@ -1,34 +1,58 @@
-//singleton
+// //singleton
 
-const JsUser = {
-    name:"siddhika",
-    "full name":'siddhika rajput',  //only square notation can be used to access
-    age:"19",
-    city:"Faizpur",
-    email:"siddhika129@gmail.com"
+// const JsUser = {
+//     name:"siddhika",
+//     "full name":'siddhika rajput',  //only square notation can be used to access
+//     age:"19",
+//     city:"Faizpur",
+//     email:"siddhika129@gmail.com"
+// }
+
+// // console.log(JsUser.name);
+// // console.log(JsUser["name"]);
+
+// JsUser.email = "siddhika243@gmail.com"
+// console.log(JsUser.email);
+// // Object.freeze(JsUser)   //is vjh se object freeze kr diya
+
+
+// JsUser.email = "sanju123@gmail.com"
+// console.log(JsUser)
+
+// JsUser.greeting = function(){
+//     console.log(`Good Morning! ,${this.name}`);
+// }    //...ye bolra ki ye function nhiyy kyuki object freeze kr liya
+
+// console.log(JsUser.greeting())
+
+// const  appUser = {}
+
+// appUser.id = "sidraj"
+// appUser.name= "siddhika"
+// appUser.isLoggedIn = false
+
+// console.log(appUser)
+
+const regularUser = {
+    email: "siddhika123gmail.com",
+    fullname: {
+        userFullName: {
+        firstName: "siddhika",
+        lastName: "rajput"
+        }
+    }
 }
 
-// console.log(JsUser.name);
-// console.log(JsUser["name"]);
+console.log(regularUser.fullname.userFullName);
 
-JsUser.email = "siddhika243@gmail.com"
-console.log(JsUser.email);
-// Object.freeze(JsUser)   //is vjh se object freeze kr diya
+console.log(Object);
 
+const obj1 = {1: "a" , 2: "b"}
+const obj2 = {3: "a", 4: "b"}
 
-JsUser.email = "sanju123@gmail.com"
-console.log(JsUser)
+// const obj3 = Object.assign(obj1,obj2)
+const obj3 = Object.assign({},obj1,obj2);  //curly braces is target hai jo achieve krna baki sab src
+console.log(obj3);
 
-JsUser.greeting = function(){
-    console.log(`Good Morning! ,${this.name}`);
-}    //...ye bolra ki ye function nhiyy kyuki object freeze kr liya
-
-console.log(JsUser.greeting())
-
-const  appUser = {}
-
-appUser.id = "sidraj"
-appUser.name= "siddhika"
-appUser.isLoggedIn = false
-
-console.log(appUser)
+const obj4 = {...obj1,...obj2} //use this always
+console.log(obj4);
